7417b6f9b68c89013dbd6f0908b04552c050ac74
kaviyarasan27 <verseking493@gmail.com>
2021-10-27 14:40:25 +0530

    updated Class

---
+++ src/com/git/one.java
@@ -0,0 +1,18 @@
+package com.git;
+
+public class one {
+	
+	
+	private void test2() {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	
+	private void test3() {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
